222bfc2f6cfdf3cdd6860d9e03858203de9b4f97
jiaxin <924463684@qq.com>
2017-11-17 18:36:32 +0800

    file输出提示和照片点击放大功能

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 #### 4.ionic serve
 #### 5.用到的插件要装，按顺序如下 
 
-##### 1.扫描插件cszbar（三个扫描插件互相会冲突，安装新的要移除前一个，移除命令为ionic cordova plugin rm）
+#### 1.扫描插件cszbar（三个扫描插件互相会冲突，安装新的要移除前一个，移除命令为ionic cordova plugin rm）
 ##### $ ionic cordova plugin add cordova-plugin-cszbar
 ##### $ npm install --save @ionic-native/zbar
 #####  barcodescanner
@@ -15,14 +15,18 @@
 ##### $ ionic cordova plugin add cordova-plugin-qrscanner
 ##### $ npm install --save @ionic-native/qr-scanner
 
-##### 2.内部浏览器插件
+#### 2.内部浏览器插件
 ##### $ ionic cordova plugin add cordova-plugin-inappbrowser
 ##### $ npm install --save @ionic-native/in-app-browser
 
-##### 3.拍照插件
+#### 3.拍照插件
 ##### $ ionic cordova plugin add cordova-plugin-camera
 ##### $ npm install --save @ionic-native/camera
 
-##### 4.打电话插件
+#### 4.打电话插件
 ##### $ ionic cordova plugin add call-number
 ##### $ npm install --save @ionic-native/call-number
+
+#### 5.FILE插件
+##### $ ionic cordova plugin add cordova-plugin-file
+##### $ npm install --save @ionic-native/file
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,26 @@ import { File } from '@ionic-native/file';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  // 第一次点击时间
+  temp: any;
+  // 第二次点击时间
+  tempTime: any;
+  // 单击事件倒计时
+  timeout: any;
+  // 是否放大
+  isZoom: boolean = false;
+  // 是否查看大图
+  isWatchDetail: boolean = false;
+  // 是否在移动图片
+  isPan: boolean = false;
+  // 初始值
+  initalX: number = 0;
+  initalY: number = 0;
+  // 图片集合
+  imgsrcs: any = [
+    { name: '图1', src: 'assets/imgs/logo.png' },
+    { name: '图2', src: 'assets/icon/favicon.ico' }
+  ]
   constructor(
     public navCtrl: NavController,
     private zbar: ZBar,
@@ -53,17 +72,92 @@ export class HomePage {
   checkFile() {
     this.file
       .checkDir(this.file.dataDirectory, 'mydir')
-      .then(_ => console.log('文件夹已存在'))
+      .then(_ => alert('文件夹存在' + this.file.dataDirectory))
       .catch(err => {
         this.createFile();
-        console.log('文件夹不存在')
+        alert('文件夹不存在' + this.file.dataDirectory)
       });
   }
   // 创建文件夹
   createFile() {
     this.file
       .createDir(this.file.dataDirectory, 'mydir', true)
-      .then(_ => console.log('成功创建文件夹'))
-      .catch(err => console.log('失败'))
+      .then(_ => alert('成功创建文件夹'))
+      .catch(err => alert('失败'))
+  }
+
+  //单击显示图片滑动页
+  showImage(ev: any) {
+    let imgScale = ev.target;
+    imgScale.style.transition = 'transform 0.33s';
+    this.isWatchDetail = !this.isWatchDetail;
+  }
+  //单击关闭图片滑动页，需要判断是不是双击
+  doClick(ev: any) {
+    // // 
+    clearTimeout(this.timeout);
+    if (!this.temp) {
+      this.temp = new Date();
+    } else {
+      this.tempTime = this.temp;
+      this.temp = new Date();
+    }
+    //单击事件倒计时
+    this.timeout = setTimeout(() => {
+      this.slideDown(ev);
+    }, 400);
+    //两次点击时间差
+    let clickBuffer = this.temp - this.tempTime;
+    //双击事件触发
+    if (clickBuffer && clickBuffer < 400) {
+      this.doDoubleClick(ev);
+      clearTimeout(this.timeout);
+    }
+
+
+  }
+  //双击放大缩小
+  doDoubleClick(ev: any) {
+    //获取事件对象
+    console.log("double");
+    let imgScale = ev.target;
+    imgScale.style.transition = 'transform 0.33s';
+
+    if (!this.isZoom) {
+      imgScale.style.transform = 'scale(2)';
+    } else {
+      imgScale.style.transform = 'scale(1)';
+    }
+    this.isZoom = !this.isZoom;
+  }
+  // 回退
+  slideDown(ev: any) {
+    let imgScale = ev.target;
+    if (this.isZoom) {
+      imgScale.style.transition = 'transform 0.33s';
+      imgScale.style.transform = 'scale(1)';
+    } else {
+      setTimeout(() => {
+        this.isWatchDetail = !this.isWatchDetail;
+      }, 400);
+    }
+    this.isZoom = !this.isZoom;
+    this.isPan = !this.isPan;
+
+  }
+  // 移动图片
+  panMove(ev: any) {
+    if (this.isZoom) {
+      this.isPan = !this.isPan;
+      console.log("pan");
+      let imgWrap = ev.target.parentElement.parentElement;
+
+      let evX = ev.deltaX;
+      let evY = ev.deltaY;
+      if (this.isWatchDetail && this.isPan) {
+        imgWrap.style.transform = 'translate(' + (evX + this.initalX) + 'px,' + (evY + this.initalY) + 'px)';
+        imgWrap.style.transition = 'transform 0s';
+      }
+    }
   }
 }
